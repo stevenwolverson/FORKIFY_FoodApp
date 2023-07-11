1c24acd3c247fe58524bcf6d1c2e92c01fb45bc4
steven <stevenwolverson2022@gmail.com>
2023-07-11 17:54:10 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1123fd21-49aa-47f0-8e32-d689fe71f248';
+export const MODAL_CLOSE_SEC = 2.5;
